b08ec6c82976d91f5326451642234f37e01266b5
clessmax <vuongvantruong1987@gmail.com>
2015-05-03 15:58:40 +0700

    add

--- src/com/vnp/core/base/BaseFragment.java
+++ src/com/vnp/core/base/BaseFragment.java
@@ -2,21 +2,31 @@ package com.vnp.core.base;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+	public abstract int getLayOut();
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View v = inflater.inflate(getLayOut(), null);
+		init(v);
+		return v;
+	}
+
+	public abstract void init(View v);
 
 	public BaseFragment() {
+		super();
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
 	}
 
 }
