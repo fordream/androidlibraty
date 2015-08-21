028de1c4e6bf22d3ffbd49fe705265341b687c69
clessmax <vuongvantruong1987@gmail.com>
2015-08-21 11:33:27 +0700

    add

--- src/com/vnp/core/common/CommonAndroid.java
+++ src/com/vnp/core/common/CommonAndroid.java
@@ -400,6 +400,13 @@ public class CommonAndroid {
 		}
 	}
 
+	public static String bitmapToBase64(Bitmap bitmap) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+		byte[] b = baos.toByteArray();
+		return Base64.encodeToString(b, Base64.DEFAULT);
+	}
+
 	/**
 	 * 
 	 * @param cursor
