ae596febfee834d02249b7c1467436f633859716
clessmax <vuongvantruong1987@gmail.com>
2015-09-07 21:20:12 +0700

    addd

---
+++ src/com/vnp/core/service/asyn/VNPServiceAsynTask.java
@@ -0,0 +1,71 @@
+package com.vnp.core.service.asyn;
+
+import java.util.Set;
+
+import org.json.JSONObject;
+
+import com.vnp.core.service.RequestMethod;
+import com.vnp.core.service.RestClient;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class VNPServiceAsynTask extends AsyncTask<String, String, String> {
+	private ProgressDialog progressDialog;
+
+	public void start() {
+		execute("");
+	}
+
+	private Context context;
+	private String api;
+
+	public Context getContext() {
+		return context;
+	}
+
+	public VNPServiceAsynTask(Context context, String api) {
+		this.context = context;
+		this.api = api;
+		progressDialog = createProgressDialog();
+	}
+
+	public ProgressDialog createProgressDialog() {
+		return null;
+	}
+
+	public String getServiceUrl() {
+		return "";
+	}
+
+	public Bundle createBundle() {
+		Bundle extras = new Bundle();
+		return extras;
+	}
+
+	public void run() {
+	}
+
+	public void finisḥ(ExecuteAPI execute) {
+
+	}
+
+	public void run(ExecuteAPI execute) {
+
+	}
+
+	// =============================================================================
+	public ExecuteAPI execute(Bundle extras, String api) {
+		ExecuteAPI executeAPI = new ExecuteAPI(getServiceUrl());
+		executeAPI.execute(extras, api);
+		return executeAPI;
+	}
+
+	@Override
+	final public String doInBackground(String... params) {
+		run();
+		return null;
+	}
+}
