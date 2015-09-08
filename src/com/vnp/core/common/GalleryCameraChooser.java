3dd7f7f28b3942ef49cac880b0e56a6b82fe2c56
clessmax <vuongvantruong1987@gmail.com>
2015-09-08 13:45:57 +0700

    adđ

--- src/com/vnp/core/common/GalleryCameraChooser.java
+++ src/com/vnp/core/common/GalleryCameraChooser.java
@@ -1,7 +1,8 @@
 package com.vnp.core.common;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Set;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,8 +12,8 @@ import android.net.Uri;
 
 public abstract class GalleryCameraChooser {
 
-	public static final int REQUESTCODEGALLERY = 10807;
-	public static final int REQUESTCODECAMERA = 10808;
+	public static final int REQUESTCODEGALLERY = 107;
+	public static final int REQUESTCODECAMERA = 106;
 
 	public void startGalleryChooser(Activity activity) {
 		Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
@@ -20,10 +21,29 @@ public abstract class GalleryCameraChooser {
 		activity.startActivityForResult(photoPickerIntent, REQUESTCODEGALLERY);
 	}
 
-	
 	public void startCameraChooser(Activity activity) {
 		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//		if (hasImageCaptureBug()) {
+//			cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("/sdcard/tmp")));
+//		} else {
+//			cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//		}
 		activity.startActivityForResult(cameraIntent, REQUESTCODECAMERA);
+	}
+
+	public boolean hasImageCaptureBug() {
+
+		// list of known devices that have the bug
+		ArrayList<String> devices = new ArrayList<String>();
+		devices.add("android-devphone1/dream_devphone/dream");
+		devices.add("generic/sdk/generic");
+		devices.add("vodafone/vfpioneer/sapphire");
+		devices.add("tmobile/kila/dream");
+		devices.add("verizon/voles/sholes");
+		devices.add("google_ion/google_ion/sapphire");
+
+		return devices.contains(android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/" + android.os.Build.DEVICE);
+
 	}
 
 	public final void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
@@ -37,7 +57,7 @@ public abstract class GalleryCameraChooser {
 			}
 		} else if (REQUESTCODECAMERA == requestCode && resultCode == Activity.RESULT_OK) {
 			onCamera((Bitmap) data.getExtras().get("data"));
-		} 
+		}
 	}
 
 	private Bitmap decodeUri(Activity activity, Uri selectedImage) throws FileNotFoundException {
