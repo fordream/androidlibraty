ebd8508c2ec413b881a840e68d3e3de75d1367fe
clessmax <vuongvantruong1987@gmail.com>
2015-09-08 17:40:30 +0700

    add

--- src/com/vnp/core/common/GalleryCameraChooser.java
+++ src/com/vnp/core/common/GalleryCameraChooser.java
@@ -2,14 +2,19 @@ package com.vnp.core.common;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.provider.MediaStore;
 
 public abstract class GalleryCameraChooser {
 
@@ -23,40 +28,84 @@ public abstract class GalleryCameraChooser {
 		releaseCamera();
 	}
 
+	private String outfileName;
+	private Uri uriOutfileName;
+
 	public void startCameraChooser(Activity activity) {
+		VnpMemoryUtils memoryUtils = new VnpMemoryUtils(activity);
+		new File(memoryUtils.getPathCacheExternalMemory()).mkdirs();
+		outfileName = memoryUtils.getPathCacheExternalMemory() + System.currentTimeMillis() + "xouttemp.png";
+		uriOutfileName = Uri.fromFile(new File(outfileName));
 		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriOutfileName);
 		activity.startActivityForResult(cameraIntent, REQUESTCODECAMERA);
 	}
 
-	public boolean hasImageCaptureBug() {
-
-		// list of known devices that have the bug
-		ArrayList<String> devices = new ArrayList<String>();
-		devices.add("android-devphone1/dream_devphone/dream");
-		devices.add("generic/sdk/generic");
-		devices.add("vodafone/vfpioneer/sapphire");
-		devices.add("tmobile/kila/dream");
-		devices.add("verizon/voles/sholes");
-		devices.add("google_ion/google_ion/sapphire");
-
-		return devices.contains(android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/" + android.os.Build.DEVICE);
-
+	private static int exifToDegrees(int exifOrientation) {
+		if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+			return 90;
+		} else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+			return 180;
+		} else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+			return 270;
+		}
+		return 0;
 	}
 
 	public final void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
 		if (REQUESTCODEGALLERY == requestCode && resultCode == Activity.RESULT_OK) {
 			Uri selectedImage = data.getData();
-
+			Bitmap bitmap = null;
 			try {
-				onGallery(decodeUri(activity, selectedImage));
-			} catch (FileNotFoundException e) {
-				onGalleryError();
-			}
-		} else if (REQUESTCODECAMERA == requestCode && resultCode == Activity.RESULT_OK) {
-			onCamera((Bitmap) data.getExtras().get("data"));
+				bitmap = decodeUri(activity, selectedImage);
+				int orientation = getOrientation(activity, selectedImage);
+				
+				ExifInterface exif = new ExifInterface(selectedImage.getPath());
+				int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
 
+				int rotationInDegrees = exifToDegrees(rotation);
+				bitmap = Bitmap90(bitmap, orientation);
+			} catch (Exception e1) {
+			}
+			onGallery(bitmap);
+		} else if (REQUESTCODECAMERA == requestCode && resultCode == Activity.RESULT_OK) {
+
+			Bitmap bitmap = null;
+			try {
+				bitmap = decodeUri(activity, uriOutfileName);
+				ExifInterface exif = new ExifInterface(uriOutfileName.getPath());
+				int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+				int rotationInDegrees = exifToDegrees(rotation);
+				bitmap = Bitmap90(bitmap, rotationInDegrees);
+			} catch (IOException e1) {
+			}
+			// Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+			onCamera(bitmap);
 			releaseCamera();
+			// new File(outfileName).delete();
 		}
+	}
+
+	public static int getOrientation(Context context, Uri photoUri) {
+		/* it's on the external media. */
+		Cursor cursor = context.getContentResolver().query(photoUri, new String[] { MediaStore.Images.ImageColumns.ORIENTATION }, null, null, null);
+
+		if (cursor.getCount() != 1) {
+			return -1;
+		}
+
+		cursor.moveToFirst();
+		return cursor.getInt(0);
+	}
+
+	private Bitmap Bitmap90(Bitmap bitmap, int r) {
+		LogUtils.e("rrrr", r + "");
+		Matrix matrix = new Matrix();
+		matrix.postRotate(r);
+		Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+		Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+		return rotatedBitmap;
 	}
 
 	private void releaseCamera() {
@@ -72,7 +121,7 @@ public abstract class GalleryCameraChooser {
 
 		} catch (Error er) {
 
-		}		
+		}
 	}
 
 	private Bitmap decodeUri(Activity activity, Uri selectedImage) throws FileNotFoundException {
