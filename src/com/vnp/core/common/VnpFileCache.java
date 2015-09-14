e542918e75532ff2b082e9267a34c84f4b83a272
clessmax <vuongvantruong1987@gmail.com>
2015-09-14 23:26:25 +0700

    add vnpFileCache

--- src/com/vnp/core/common/VnpFileCache.java
+++ src/com/vnp/core/common/VnpFileCache.java
@@ -1,8 +1,11 @@
 package com.vnp.core.common;
 
 import java.io.File;
+import java.io.IOException;
 
 import android.content.Context;
+import android.os.Environment;
+import android.os.StatFs;
 
 public class VnpFileCache {
 	private File cacheDir;
@@ -18,6 +21,15 @@ public class VnpFileCache {
 		if (!cacheDir.exists()) {
 			cacheDir.mkdirs();
 		}
+
+		/**
+		 * add new by tvuong
+		 */
+
+		pathFileExternalMemory = Environment.getExternalStorageDirectory() + "/Android/data/" + context.getPackageName() + "/files/";
+		pathCacheExternalMemory = Environment.getExternalStorageDirectory() + "/Android/data/" + context.getPackageName() + "/cache/";
+		pathFileInternalMemory = context.getFilesDir().getPath() + "/";
+
 	}
 
 	public File getFile(String url) {
@@ -25,7 +37,6 @@ public class VnpFileCache {
 		String filename = String.valueOf(url.hashCode());
 		File f = new File(cacheDir, filename);
 		return f;
-
 	}
 
 	public void clear() {
@@ -33,5 +44,61 @@ public class VnpFileCache {
 		for (File f : files) {
 			f.delete();
 		}
+	}
+
+	/**
+	 * add new by tvuong
+	 */
+	/** 1MB block. */
+	private static final int MB = 1048576;
+	private String pathFileExternalMemory = "";
+	private String pathCacheExternalMemory = "";
+	private String pathFileInternalMemory = "";
+
+	public String getPathFileExternalMemory() {
+		return pathFileExternalMemory;
+	}
+
+	public String getPathCacheExternalMemory() {
+		return pathCacheExternalMemory;
+	}
+
+	public String getPathFileInternalMemory() {
+		return pathFileInternalMemory;
+	}
+
+	public boolean checkSDAvaiable(String path) {
+		boolean isSDCardExisted = false;
+		if (!new File(path).exists()) {
+			new File(path).mkdirs();
+		}
+
+		File file = new File(path, "test" + System.currentTimeMillis() + ".txt");
+
+		try {
+			isSDCardExisted = file.createNewFile();
+		} catch (IOException e1) {
+			isSDCardExisted = false;
+		}
+		file.delete();
+		return isSDCardExisted;
+	}
+
+	public double checkSDFreeMB() {
+
+		if (!checkSDAvaiable(getPathCacheExternalMemory())) {
+			return 0;
+		} else {
+			StatFs stat = new StatFs(getPathCacheExternalMemory());
+			double sdAvailSize = (double) stat.getAvailableBlocks() * (double) stat.getBlockSize();
+
+			return sdAvailSize / MB;
+		}
+	}
+
+	public long avaiableInternalStoreMemorySize() {
+		StatFs stat = new StatFs(Environment.getDataDirectory().getPath());
+		long bytesAvailable = (long) stat.getFreeBlocks() * (long) stat.getBlockSize();
+		return (bytesAvailable / 1048576);
 	}
 }
